ebc6f37f18323b39755e105dc1772514ac6fd324
Yuki Esaki <yesaki610@gmail.com>
2020-05-11 15:03:11 +0900

    電卓アプリ

---
+++ calculator/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by esaki yuki on 2020/05/08.
+//  Copyright © 2020 esaki yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var number1: Int = 0
+    var number2: Int = 0
+    var number3: Int = 0
+    
+    var ope: Int = 0
+    
+    @IBAction func select3() {
+        number1 = number1 * 10 + 3
+        label.text = String(number1)
+    }
+    
+    @IBAction func select4() {
+        number1 = number1 * 10 + 4
+        label.text = String(number1)
+    }
+    
+    @IBAction func plas() {
+        number2 = number1
+        number1 = 0
+        ope = 1
+    }
+    
+    @IBAction func minus() {
+        number2 = number1
+        number1 = 0
+        ope = 2
+    }
+    
+    @IBAction func kakeru() {
+        number2 = number1
+        number1 = 0
+        ope = 3
+    }
+    
+    @IBAction func waru() {
+        number2 = number1
+        number1 = 0
+        ope = 4
+    }
+    
+    @IBAction func equal() {
+        
+        if ope == 1 {
+            number3 = number2 + number1
+        } else if ope == 2 {
+            number3 = number2 - number1
+        }else if ope == 3 {
+            number3 = number2 * number1
+        }else if ope == 4 {
+            number3 = number2 / number1
+        }
+        
+        label.text = String(number3)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
